aff3a576f130dc81e05b985e19a7e29a4e33e0ad
Saeed Halab <halab.saeed@gmail.com>
2022-11-26 06:41:48 -0800

    Implement ZombieHelper contract

---
+++ contracts/ZombieHelper.sol
@@ -0,0 +1,33 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+import "./ZombieFeeding.sol";
+
+contract ZombieHelper is ZombieFeeding {
+    modifier aboveLevel(uint256 _zombieId, uint256 _level) {
+        require(zombies[_zombieId].level >= _level);
+        _;
+    }
+
+    function changeName(uint256 _zombieId, string calldata _newName)
+        external
+        aboveLevel(_zombieId, 2)
+    {
+        require(zombieToOwner[_zombieId] == msg.sender);
+        zombies[_zombieId].name = _newName;
+    }
+
+    function changeDna(uint256 _zombieId, uint256 _newDna)
+        external
+        aboveLevel(_zombieId, 20)
+    {
+        require(zombieToOwner[_zombieId] == msg.sender);
+        zombies[_zombieId].dna = _newDna;
+    }
+
+    function getZombiesByOwner(address _owner)
+        external
+        view
+        returns (uint256[] memory)
+    {}
+}
